c227d59e0596279df39efc9517001ce9f32d7fae
shoushinei <shoushinei@gmail.com>
2024-06-02 00:52:45 +0900

    ブログ投稿の編集

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <title>Blog</title>
+         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
     <body>
         <h1>Blog Name</h1>
---
+++ resources/views/posts/edit.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE HTML>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Posts</title>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    </head>
+<body>
+    <h1 class="title">編集画面</h1>
+    <div class="content">
+        <form action="/posts/{{ $post->id }}" method="POST">
+            @csrf
+            @method('PUT')
+            <div class='content__title'>
+                <h2>タイトル</h2>
+                <input type='text' name='post[title]' value="{{ $post->title }}">
+                <p class="title__error" style="color:red">{{ $errors->first('post.title') }}</p>
+            </div>
+            <div class='content__body'>
+                <h2>本文</h2>
+                <input type='text' name='post[body]' value="{{ $post->body }}">
+                <p class="body__error" style="color:red">{{ $errors->first('post.body') }}</p>
+            </div>
+            <input type="submit" value="update">
+        </form>
+        <div class="back">[<a href="/posts/{{ $post->id }}">戻る</a>]</div>
+    </div>
+</body>
+</html>
